9d453d372ec98b54c29d79000e36498a93945c11
destinym <mengliang@aliyun.com>
2015-01-24 17:14:06 +0800

    modify css

--- wordpress/wp-content/themes/tutorial/footer.php
+++ wordpress/wp-content/themes/tutorial/footer.php
@@ -2,14 +2,18 @@
 
 <div class="footer">
 <div class="footer_2">
-	 <div class="footer_first">	
-	 TEL: 0871-67323796 PHONE: 13808702687 FAX: 0871-67169581 E-MAIL: 494090538@QQ.COM
+	 <div class="footer_first" >	
+	 <div class="footer_letter"> TEL: 0871-67323796</div>
+	 <div class="footer_letter"> PHONE: 13808702687 </div>
+	 <div class="footer_letter"> FAX: 0871-67169581</div>
+	 <div class="footer_letter">  E-MAIL: 494090538@QQ.COM</div>
+	                           
 	 </div>
 
 
 	 
 	 <div class ="footer_second">
-	   昆明杰兴瑞企业营销策划有限公司    版权所有    滇ICP备xxxxxxxx号    Design By：杰兴瑞
+	   昆明杰兴瑞企业营销策划有限公司    版权所有    滇ICP备14007736号-1    Design By：杰兴瑞
 	 </div>
 	</div>
 </div>
